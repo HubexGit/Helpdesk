6df8ea3227b49d08d996643ad7dc2fcf64fd6a80
FlavioBame <flaviobame66@gmail.com>
2025-10-06 13:40:33 +0200

    Added : symbol to the form2

--- Sherbime FSHZH/Form2.Designer.cs
+++ Sherbime FSHZH/Form2.Designer.cs
@@ -56,7 +56,7 @@
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(459, 29);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Zgjidhni një kategori për të vazhduar";
+            this.label3.Text = "Zgjidhni një kategori për të vazhduar:";
             // 
             // button1
             // 
